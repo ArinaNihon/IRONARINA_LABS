b228d69538f799f984d2a7a5909c0fc1386fb7bd
ArinaNihon <akitokyo@mail.ru>
2022-10-31 12:40:33 +0100

    SQL_Intro

---
+++ SQL/lab_1_intro.sql
@@ -0,0 +1,8 @@
+select * from sakila.actor;
+select * from sakila.adress;
+select * from sakila.category;
+select title from sakila.film;
+select distinct name as language_new from sakila.language;
+select * from sakila.store;
+select * from sakila.staff;
+select first_name from sakila.staff;
